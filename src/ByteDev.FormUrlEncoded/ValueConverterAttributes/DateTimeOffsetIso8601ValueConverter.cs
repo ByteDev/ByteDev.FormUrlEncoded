fa57dd509c2c55f807f631425fc129df83398db3
Tim Nixon <tnixon01@gmail.com>
2024-10-22 19:16:00 -0400

    Added converters for DateTimeOffset to/from ISO8601 and ticks.

---
+++ src/ByteDev.FormUrlEncoded/ValueConverterAttributes/DateTimeOffsetIso8601ValueConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace ByteDev.FormUrlEncoded.ValueConverterAttributes
+{
+    /// <summary>
+    /// <see cref="FormUrlEncodedValueConverterAttribute"/> that converts between an 
+    /// <see href="https://www.iso.org/iso-8601-date-and-time-format.html">ISO8601</see>-formatted string 
+    /// and <see cref="DateTimeOffset"/> types.
+    /// </summary>
+    public class DateTimeOffsetIso8601ValueConverter : FormUrlEncodedValueConverterAttribute
+    {
+        public override object ConvertFromString(string value)
+        {
+            DateTimeOffset.TryParse(value, out DateTimeOffset result);
+            return result;
+        }
+
+        public override string ConvertToString(object value)
+        {
+            var dto = (DateTimeOffset)value;
+            var str = dto.ToString("o", CultureInfo.InvariantCulture);
+            return str;
+        }
+    }
+}
---
+++ src/ByteDev.FormUrlEncoded/ValueConverterAttributes/DateTimeOffsetUnixTicksValueConverterAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ByteDev.FormUrlEncoded.ValueConverterAttributes
+{
+    /// <summary>
+    /// <see cref="FormUrlEncodedValueConverterAttribute"/> that converts between a string represeting
+    /// <see href="https://learn.microsoft.com/en-us/dotnet/api/system.datetime.ticks">Ticks</see>
+    /// and <see cref="DateTimeOffset"/> types.
+    /// </summary>
+    public class DateTimeOffsetUnixTicksValueConverter : FormUrlEncodedValueConverterAttribute
+    {
+        public override object ConvertFromString(string value)
+        {
+            var ticks = Convert.ToInt64(value);
+            return new DateTimeOffset(ticks, TimeSpan.Zero);
+        }
+
+        public override string ConvertToString(object value)
+        {
+            var dto = (DateTimeOffset)value;
+            return dto.Ticks.ToString();
+        }
+    }
+}
